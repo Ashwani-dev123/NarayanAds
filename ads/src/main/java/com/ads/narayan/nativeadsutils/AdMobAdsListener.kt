5580544a5638393d9afef03077e00313879c92ab
Ashwani <ashwani.narayaninfotech@gmail.com>
2025-02-07 11:58:08 +0530

    Change class name

--- ads/src/main/java/com/ads/narayan/nativeadsutils/AdMobAdsListener.kt
+++ ads/src/main/java/com/ads/narayan/nativeadsutils/AdMobAdsListener.kt
@@ -2,7 +2,7 @@
 
 package com.ads.narayan.nativeadsutils
 
-interface NativeAdLoadCallback<T> {
+interface AdMobAdsListener<T> {
 
     /**
      * This method is called when your AD data was loaded successfully
--- ads/src/main/java/com/ads/narayan/nativeadsutils/AdStatusModel.kt
+++ ads/src/main/java/com/ads/narayan/nativeadsutils/AdStatusModel.kt
@@ -8,7 +8,7 @@ import com.google.android.gms.ads.AdListener
 data class AdStatusModel<T>(
     var loadedAd: T? = null,
     var adID: String = "",
-    var listener: NativeAdLoadCallback<T>? = null,
+    var listener: AdMobAdsListener<T>? = null,
     var isAdLoadingRunning: Boolean = false,
     var defaultAdListener: AdListener? = null
 )
